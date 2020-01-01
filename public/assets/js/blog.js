7b48f215f14d5b1e2c38d33ca7a8fb60aa8968a6
Hasan Emre Tonguç <hasanemretonguc@gmail.com>
2020-01-01 05:08:31 +0300

    thumbnail select

--- public/assets/js/blog.js
+++ public/assets/js/blog.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	
-	/* ======= Highlight.js Plugin ======= */ 
-    /* Ref: https://highlightjs.org/usage/ */     
     $('pre code').each(function(i, block) {
 	    hljs.highlightBlock(block);
 	 });
--- public/assets/js/imageviewer.js
+++ public/assets/js/imageviewer.js
@@ -1,5 +1,40 @@
-$("#thumbSelect").click(() => {
-    alert("MERHABALAR AQ");
+// SEC DEDIGI ZAMAN RESIMLER ACILACAK KUCULECEK!!
+
+// EN YUKARDAKI RESMIN UZEIRNDEKI TUS
+var thumbButton = document.getElementById('thumbSelect');
+// RESIM LISTESININ OLDUGU BOLUM
+var imageView = document.getElementById('imgPlate');
+// RESIMLERI LISTE
+var pics = document.getElementsByName('imgs');
+// THUMBNAILI VE KONUMU ALDIGIM GIZLI INPUTU AL
+var thumbnail = document.getElementById('image');
+var thumbnail_loc = document.getElementById('image_location');
+
+// RESIM LISTESINI ACIP KAPATIYOR 
+thumbButton.addEventListener('click', toggleView);
+
+// BUTUN RESIMLERE TIKLANDIGI ZAMAN RENK VERIYOR
+pics.forEach(element => {
+    element.addEventListener('click', () => {
+        // DIGER RESIMLERDEN SECIMI KALDIRIYOR
+        pics.forEach(element => {
+            element.classList.remove('select');
+        });
+        // SADECE BU SECIME RENK VERIYOR
+        element.classList.add('select');
+        // VIEW BOLUMUNU KAPATIYOR
+        toggleView();
+        thumbnail.src = element.src;
+        thumbnail_loc.value = element.src;
+    });
 });
 
-// SEC DEDIGI ZAMAN RESIMLER ACILACAK KUCULECEK!!
+function toggleView() {
+    if (imageView.style.display === 'none') {
+        imageView.style.display = 'flex';
+	} else {
+        imageView.style.display = 'none';
+    }
+    // ANIMASYONU KAPATIP ACIYOR
+    imageView.classList.toggle('fadeIn');
+}
